16cb163c700a4351fc9046f5e17169d187f0120e
liuzhangfeiabc <50104096+liuzhangfeiabc@users.noreply.github.com>
2023-05-30 11:03:46 +0800

    test ffi

--- crates/tlsf_c_allocator/build.rs
+++ crates/tlsf_c_allocator/build.rs
@@ -1,17 +1,10 @@
 fn main() {
+    let mut base_config = cc::Build::new();
+    base_config.file("src/tlsf.c");
+
+    base_config.warnings(true).compile("libtlsf.a");
+
     println!("cargo:rustc-link-lib=static=tlsf");
     println!("cargo:rerun-if-changed=src/tlsf.c");
     println!("cargo:rerun-if-changed=src/tlsf.h");
-
-    let mut base_config = cc::Build::new();
-
-    base_config.file("src/tlsf.c");
-
-    base_config
-        .warnings(true)
-        //.flag("-static")
-        //.flag("-no-pie")
-        //.flag("-fno-builtin")
-        //.flag("-ffreestanding")
-        .compile("libtlsf.a");
 }
